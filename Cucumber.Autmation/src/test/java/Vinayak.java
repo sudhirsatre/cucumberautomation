cff8f9750c95eff521f4b1332ad9a829289cdf25
sudhirsatre <sudhirsatre@gmail.com>
2022-07-14 12:45:34 +0530

    Third commit

--- Cucumber.Autmation/src/test/java/Vinayak.java
+++ Cucumber.Autmation/src/test/java/Vinayak.java
@@ -1,4 +1,14 @@
+import org.testng.Assert;
 
 public class Vinayak {
 
+public static void main(String[] args) {
+	
+	
+	System.out.println("hi this is test");
+	Assert.assertTrue(true);
+	System.out.println();
+	
+}
+	
 }
